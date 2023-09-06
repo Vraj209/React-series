38410cf83174f20adf9cd34e0fb1fcb5483bee43
Vraj209 <80612219+Vraj209@users.noreply.github.com>
2023-09-06 12:28:34 -0400

    05 - Click Event

--- src/Content.js
+++ src/Content.js
@@ -9,9 +9,22 @@ const Content = () => {
         const i= Math.floor(Math.random() * 4)
         return name[i]
       }
+      const handleClick = ()=>{
+        console.log("You click the button")
+      }
+      const handleClick2 = (name)=>{
+        console.log(`${name} was click the button`)
+      }
+      const handleClick3 = (e)=>{
+        console.log(e.target.innerHTML)
+      }
   return (
     <main>
-      <p>Hello {handleNameChange()}</p>
+      <p onDoubleClick={handleClick}>Hello {handleNameChange()}</p>
+      <button onClick={handleClick}>Click Here</button>
+      <button onClick={()=> handleClick2('Vraj')}>Click Here</button>
+      <button onClick={(e)=> handleClick3(e)}>Click Here</button>
+
     </main>
   )
 }
